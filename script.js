f699e384cecc483dc74e8bf785b2d95ec31c4ac7
karo.morales <ca.morales19@ciaf.edu.co>
2025-10-03 18:51:50 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let foto = document.getElementById ("foto");
+let boton = document.getElementById ("boton");
+
+boton.addEventListener("click", function() {
+    if (foto.scr.endsWith = ("imagenes\images (5).jpg")) {
+        foto.scr = "imagenes\images (6).jpg";
+    
+    } else {
+    foto.scr = "imagenes\images (5).jpg"
+
+    } 
+});
